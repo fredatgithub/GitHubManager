7db50d067acee63930410f6853e7e2b3c6a278b6
fred <domaine75@gmail.com>
2026-02-18 17:48:03 +0100

    clean code

--- GitHubManager/MainWindow.xaml.cs
+++ GitHubManager/MainWindow.xaml.cs
@@ -1,30 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace GitHubManager
 {
   /// <summary>
   /// Logique d'interaction pour MainWindow.xaml
   /// </summary>
-  public partial class MainWindow: Window
+  public partial class MainWindow : Window
   {
     private readonly ObservableCollection<GitHubRepository> _repositories = new ObservableCollection<GitHubRepository>();
     private string _token;
